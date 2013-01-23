17c9d544a3c1adc20549136b5d5bad3cb18bf884
Kevin Downey <redchin@gmail.com>
2013-01-22 19:41:16 -0500

    carica and props3t

--- project.clj
+++ project.clj
@@ -6,7 +6,10 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.apache.commons/commons-compress "1.4.1"]
                  [org.apache.commons/commons-io "1.3.2"]
-                 [org.apache.commons/commons-lang3 "3.1"]]
+                 [org.apache.commons/commons-lang3 "3.1"]
+                 [props3t "0.0.3" :exclude [clj-http]]
+                 [clj-http "0.6.3"]
+                 [sonian/carica "1.0.2"]]
   :min-lein-version "2.0.0"
   :plugins [[lein-bikeshed "0.1.0"]
             [lein-swank "1.4.4"]
